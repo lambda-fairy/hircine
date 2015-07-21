dd7b4a2ea1a711447ffdc382895668bc7b356815
Chris Wong <lambda.fairy@gmail.com>
2015-07-21 15:35:50 +1200

    Add some more commands

--- Hircine/Command.hs
+++ Hircine/Command.hs
@@ -33,6 +33,21 @@ instance (IsCommand a, IsCommand b) => IsCommand (Either a b) where
     toCommand = either toCommand toCommand
 
 
+type Bytes = ByteString
+
+
+data Join = Join ![Bytes] ![Bytes]
+    deriving (Read, Show)
+
+instance IsCommand Join where
+    fromCommand (Command "JOIN" [channels, keys]) = Just $ Join (B.split ',' channels) (B.split ',' keys)
+    fromCommand (Command "JOIN" [channels]) = Just $ Join (B.split ',' channels) []
+    fromCommand _ = Nothing
+
+    toCommand (Join channels []) = Command "JOIN" [B.intercalate "," channels]
+    toCommand (Join channels keys) = Command "JOIN" [B.intercalate "," channels, B.intercalate "," keys]
+
+
 data Nick = Nick !Bytes
     deriving (Read, Show)
 
@@ -41,6 +56,26 @@ instance IsCommand Nick where
     fromCommand _ = Nothing
 
     toCommand (Nick nick) = Command "NICK" [nick]
+
+
+data Notice = Notice ![Bytes] !Bytes
+    deriving (Read, Show)
+
+instance IsCommand Notice where
+    fromCommand (Command "NOTICE" [targets, message]) = Just $ Notice (B.split ',' targets) message
+    fromCommand _ = Nothing
+
+    toCommand (Notice targets message) = Command "NOTICE" [B.intercalate "," targets, message]
+
+
+data Pass = Pass !Bytes
+    deriving (Read, Show)
+
+instance IsCommand Pass where
+    fromCommand (Command "PASS" [pass]) = Just $ Pass pass
+    fromCommand _ = Nothing
+
+    toCommand (Pass pass) = Command "PASS" [pass]
 
 
 data Ping = Ping !Bytes !(Maybe Bytes)
@@ -65,6 +100,27 @@ instance IsCommand Pong where
     toCommand (Pong server1 server2) = Command "PONG" $ server1 : maybeToList server2
 
 
+data PrivMsg = PrivMsg ![Bytes] !Bytes
+    deriving (Read, Show)
+
+instance IsCommand PrivMsg where
+    fromCommand (Command "PRIVMSG" [targets, message]) = Just $ PrivMsg (B.split ',' targets) message
+    fromCommand _ = Nothing
+
+    toCommand (PrivMsg targets message) = Command "PRIVMSG" [B.intercalate "," targets, message]
+
+
+data Quit = Quit !(Maybe Bytes)
+    deriving (Read, Show)
+
+instance IsCommand Quit where
+    fromCommand (Command "QUIT" []) = Just $ Quit Nothing
+    fromCommand (Command "QUIT" [message]) = Just $ Quit (Just message)
+    fromCommand _ = Nothing
+
+    toCommand (Quit message) = Command "QUIT" $ maybeToList message
+
+
 data User = User !Bytes !Bytes
     deriving (Read, Show)
 
@@ -73,17 +129,3 @@ instance IsCommand User where
     fromCommand _ = Nothing
 
     toCommand (User user realname) = Command "USER" [user, "0", "*", realname]
-
-
-data PrivMsg = PrivMsg ![Bytes] !Bytes
-    deriving (Read, Show)
-
-instance IsCommand PrivMsg where
-    fromCommand (Command "PRIVMSG" [targets, message]) = Just $ PrivMsg (B.split ',' targets) message
-    fromCommand _ = Nothing
-
-    toCommand (PrivMsg targets message)
-        = Command "PRIVMSG" [B.intercalate "," targets, message]
-
-
-type Bytes = ByteString
