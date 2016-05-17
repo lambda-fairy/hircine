791550eab3ed497d8c100ada7f2041024917b8c0
Chris Wong <lambda.fairy@gmail.com>
2016-05-17 17:08:04 +1200

    Unbreak command parsing code

--- Hircine/Command.hs
+++ Hircine/Command.hs
@@ -11,10 +11,11 @@
 module Hircine.Command where
 
 
+import Control.Applicative
+import Control.Monad.Trans.State
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B
-import Data.Foldable
-import Data.Maybe
+import Data.List
 import Data.Proxy
 import Data.String
 import GHC.TypeLits
@@ -40,12 +41,12 @@ instance IsCommand Command where
     toCommand = id
 
 
-newtype ParsedCommand (method :: Symbol) (params :: [*]) = ParsedCommand (List params)
+newtype ParsedCommand (method :: Symbol) params = ParsedCommand params
 
 instance forall method params. (KnownSymbol method, IsParams params) => IsCommand (ParsedCommand method params) where
-    fromCommand (Command method' params)
-        | method /= method' = Nothing
-        | otherwise = ParsedCommand <$> parseParams params
+    fromCommand (Command method' params')
+        | method == method', Just (params, []) <- runStateT parseParams params' = Just $ ParsedCommand params
+        | otherwise = Nothing
       where
         method = fromString $ symbolVal (Proxy :: Proxy method)
     toCommand (ParsedCommand params) = Command method (renderParams params)
@@ -53,87 +54,85 @@ instance forall method params. (KnownSymbol method, IsParams params) => IsComman
         method = fromString $ symbolVal (Proxy :: Proxy method)
 
 
-data List (params :: [*]) where
-    Nil :: List '[]
-    (:-) :: a -> List b -> List (a ': b)
-infixr 5 :-
-
-
-class IsParams (params :: [*]) where
-    parseParams :: [Bytes] -> Maybe (List params)
-    renderParams :: List params -> [Bytes]
-
-instance IsParams '[] where
-    parseParams [] = Just Nil
-    parseParams _ = Nothing
-    renderParams Nil = []
-
-instance (IsParam a, IsParams b) => IsParams (a ': b) where
-    parseParams (x : xs) = (:-) <$> parseParam x <*> parseParams xs
-    parseParams _ = Nothing
-    renderParams (x :- xs) = toList (renderParam x) ++ renderParams xs
-
-
-class IsParam a where
-    parseParam :: Bytes -> Maybe a
-    renderParam :: a -> Maybe Bytes
-
-instance IsParam ByteString where
-    parseParam = Just
-    renderParam = Just
-
-newtype CommaSep a = CommaSep [a]
-    deriving (Read, Show)
-
-instance IsParam a => IsParam (CommaSep a) where
-    parseParam = fmap CommaSep . traverse parseParam . B.split ','
-    renderParam (CommaSep xs) = Just $ B.intercalate "," $ mapMaybe renderParam xs
-
-instance IsParam a => IsParam (Maybe a) where
-    parseParam "" = Just Nothing
-    parseParam x = Just <$> parseParam x
-    renderParam x = x >>= renderParam
-
-
 type Bytes = ByteString
 
 
-pattern Join :: [Bytes] -> [Bytes] -> ParsedCommand "JOIN" '[CommaSep Bytes, Maybe (CommaSep Bytes)]
+class IsParams a where
+    parseParams :: StateT [Bytes] Maybe a
+    renderParams :: a -> [Bytes]
+
+instance IsParams () where
+    parseParams = pure ()
+    renderParams _ = []
+
+instance (IsParams a, IsParams b) => IsParams (a, b) where
+    parseParams = (,) <$> parseParams <*> parseParams
+    renderParams (a, b) = renderParams a ++ renderParams b
+
+instance (IsParams a, IsParams b, IsParams c) => IsParams (a, b, c) where
+    parseParams = (,,) <$> parseParams <*> parseParams <*> parseParams
+    renderParams (a, b, c) = renderParams a ++ renderParams b ++ renderParams c
+
+instance (IsParams a, IsParams b, IsParams c, IsParams d) => IsParams (a, b, c, d) where
+    parseParams = (,,,) <$> parseParams <*> parseParams <*> parseParams <*> parseParams
+    renderParams (a, b, c, d) = renderParams a ++ renderParams b ++ renderParams c ++ renderParams d
+
+instance (IsParams a, IsParams b, IsParams c, IsParams d, IsParams e) => IsParams (a, b, c, d, e) where
+    parseParams = (,,,,) <$> parseParams <*> parseParams <*> parseParams <*> parseParams <*> parseParams
+    renderParams (a, b, c, d, e) = renderParams a ++ renderParams b ++ renderParams c ++ renderParams d ++ renderParams e
+
+instance IsParams ByteString where
+    parseParams = StateT uncons
+    renderParams x = [x]
+
+newtype CommaSep = CommaSep [Bytes]
+    deriving (Read, Show)
+
+instance IsParams CommaSep where
+    parseParams = CommaSep . B.split ',' <$> StateT uncons
+    renderParams (CommaSep xs) = [B.intercalate "," xs]
+
+instance IsParams a => IsParams (Maybe a) where
+    parseParams = optional parseParams
+    renderParams = foldMap renderParams
+
+
+pattern Join :: [Bytes] -> [Bytes] -> ParsedCommand "JOIN" (CommaSep, Maybe CommaSep)
 pattern Join channels keys <-
     ParsedCommand (
-        CommaSep channels :-
+        CommaSep channels,
         ((\keys' -> case keys' of
             Just (CommaSep xs) -> xs
             Nothing -> []
-        ) -> keys) :-
-        Nil)
+        ) -> keys))
   where
-    Join channels keys = ParsedCommand (CommaSep channels :- packKeys keys :- Nil)
+    Join channels keys = ParsedCommand (CommaSep channels, packKeys keys)
       where
         packKeys [] = Nothing
         packKeys xs = Just (CommaSep xs)
 
-pattern Nick :: Bytes -> ParsedCommand "NICK" '[Bytes]
-pattern Nick nick = ParsedCommand (nick :- Nil)
+pattern Nick :: Bytes -> ParsedCommand "NICK" Bytes
+pattern Nick nick = ParsedCommand nick
 
-pattern Notice :: [Bytes] -> Bytes -> ParsedCommand "NOTICE" '[CommaSep Bytes, Bytes]
-pattern Notice targets message = ParsedCommand (CommaSep targets :- message :- Nil)
+pattern Notice :: [Bytes] -> Bytes -> ParsedCommand "NOTICE" (CommaSep, Bytes)
+pattern Notice targets message = ParsedCommand (CommaSep targets, message)
 
-pattern Pass :: Bytes -> ParsedCommand "PASS" '[Bytes]
-pattern Pass pass = ParsedCommand (pass :- Nil)
+pattern Pass :: Bytes -> ParsedCommand "PASS" Bytes
+pattern Pass pass = ParsedCommand pass
 
-pattern Ping :: Bytes -> Maybe Bytes -> ParsedCommand "PING" '[Bytes, Maybe Bytes]
-pattern Ping server1 server2 = ParsedCommand (server1 :- server2 :- Nil)
+pattern Ping :: Bytes -> Maybe Bytes -> ParsedCommand "PING" (Bytes, Maybe Bytes)
+pattern Ping server1 server2 = ParsedCommand (server1, server2)
 
-pattern Pong :: Bytes -> Maybe Bytes -> ParsedCommand "PONG" '[Bytes, Maybe Bytes]
-pattern Pong server1 server2 = ParsedCommand (server1 :- server2 :- Nil)
+pattern Pong :: Bytes -> Maybe Bytes -> ParsedCommand "PONG" (Bytes, Maybe Bytes)
+pattern Pong server1 server2 = ParsedCommand (server1, server2)
 
-pattern PrivMsg :: [Bytes] -> Bytes -> ParsedCommand "PRIVMSG" '[CommaSep Bytes, Bytes]
-pattern PrivMsg targets message = ParsedCommand (CommaSep targets :- message :- Nil)
+pattern PrivMsg :: [Bytes] -> Bytes -> ParsedCommand "PRIVMSG" (CommaSep, Bytes)
+pattern PrivMsg targets message = ParsedCommand (CommaSep targets, message)
 
-pattern Quit :: Maybe Bytes -> ParsedCommand "QUIT" '[Maybe Bytes]
-pattern Quit message = ParsedCommand (message :- Nil)
+pattern Quit :: Maybe Bytes -> ParsedCommand "QUIT" (Maybe Bytes)
+pattern Quit message = ParsedCommand message
 
-pattern User :: Bytes -> Bytes -> ParsedCommand "USER" '[Bytes, Bytes, Bytes, Bytes]
-pattern User user realname <- ParsedCommand (user :- _ :- _ :- realname :- Nil) where
-    User user realname = ParsedCommand (user :- "0" :- "*" :- realname :- Nil)
+pattern User :: Bytes -> Bytes -> ParsedCommand "USER" (Bytes, Bytes, Bytes, Bytes)
+pattern User user realname <- ParsedCommand (user, _, _, realname)
+  where
+    User user realname = ParsedCommand (user, "0", "*", realname)
