e0922f62819f9f8798a653ce59fb436cbfb49923
Chris Wong <lambda.fairy@gmail.com>
2017-06-11 22:52:30 +1200

    Brigitte: add rate limiting for Hackagebot (untested)

--- brigitte/Hackagebot.hs
+++ brigitte/Hackagebot.hs
@@ -5,10 +5,13 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
 import Data.Default.Class
+import Data.Foldable
 import Data.IORef
 import qualified Data.Text as Text
 import Network.Connection
 import Network.HTTP.Client
+import System.Clock
+import System.Timeout
 import Text.RSS.Import
 import Text.RSS.Syntax
 import Text.XML.Light.Input
@@ -21,6 +24,8 @@ import Utils
 main :: IO ()
 main = do
     crateMap <- newIORef defaultCrateMap
+    channelIdle <- newIORef =<< getMonotonicTime
+    newCrates <- newChan
     startBot params myNick $ \channel man -> forever $ do
         Message origin command <- receive
         return command
@@ -29,9 +34,14 @@ main = do
             ? (\(Mode nick _) ->
                 when (nick == myNick) $ do
                     send $ Join [channel] Nothing
-                    _ <- fork $ checkNewCrates channel crateMap man
+                    _ <- fork . liftIO $ checkNewCrates crateMap newCrates man
+                    _ <- fork . liftIO $ notifyNewCrates newCrates channelIdle channel
                     send $ PrivMsg [channel] "\x01\&ACTION Hello, world!\x01"
                     return () )
+            ? (\(PrivMsg targets _) ->
+                when (channel `elem` targets) $ liftIO $ do
+                    currentTime <- getMonotonicTime
+                    atomicWriteIORef channelIdle $! currentTime + channelIdleDelay )
   where
     myNick = "hackagebot"
     params = ConnectionParams
@@ -42,12 +52,11 @@ main = do
         }
 
 
-checkNewCrates :: ByteString -> IORef CrateMap -> Manager -> Hircine ()
-checkNewCrates _channel crateMap man = forever $ do
-    changedCrates <- liftIO $ fetchAndUpdateCrateMap feedUrl parseCrates crateMap man
-    -- TODO
-    liftIO $ print changedCrates
-    liftIO . threadDelay $ 60 * 1000 * 1000  -- 60 seconds
+checkNewCrates :: IORef CrateMap -> Chan Crate -> Manager -> IO a
+checkNewCrates crateMap newCrates man = forever $ do
+    changedCrates <- fetchAndUpdateCrateMap feedUrl parseCrates crateMap man
+    writeList2Chan newCrates changedCrates
+    threadDelay $ 60 * 1000 * 1000  -- 60 seconds
   where
     feedUrl = "https://hackage.haskell.org/packages/recent.rss"
 
@@ -61,3 +70,44 @@ checkNewCrates _channel crateMap man = forever $ do
     parseTitle title
         | [name, version] <- Text.words $ Text.pack title = Just (name, version)
         | otherwise = Nothing
+
+
+notifyNewCrates :: Chan Crate -> IORef TimeSpec -> ByteString -> IO ()
+notifyNewCrates newCrates channelIdle _channel = start
+  where
+    start = do
+        firstCrate <- readChan newCrates
+        cratesToSend <- newIORef [firstCrate]
+        startTime <- getMonotonicTime
+        loop startTime (startTime + notifyDelay) cratesToSend
+
+    loop currentTime notifyTime cratesToSend = do
+        channelIdleTime <- readIORef channelIdle
+        let timeToWait = toMicroSecs $ max notifyTime channelIdleTime - currentTime
+        if timeToWait > 0
+            then do
+                _ <- timeout timeToWait $ forever $ do
+                    crate <- readChan newCrates
+                    -- IORef operations are not interruptible, so there is no
+                    -- threat of data loss here
+                    modifyIORef' cratesToSend (crate :)
+                currentTime' <- getMonotonicTime
+                loop currentTime' notifyTime cratesToSend
+            else do
+                crates <- reverse <$> readIORef cratesToSend
+                for_ crates print  -- TODO
+                start
+
+
+channelIdleDelay :: TimeSpec
+channelIdleDelay = TimeSpec { sec = 60, nsec = 0 }
+
+notifyDelay :: TimeSpec
+notifyDelay = TimeSpec { sec = 5 * 60, nsec = 0 }
+
+
+getMonotonicTime :: IO TimeSpec
+getMonotonicTime = getTime Monotonic
+
+toMicroSecs :: TimeSpec -> Int
+toMicroSecs = ceiling . (/ (1000 :: Rational)) . fromInteger . toNanoSecs
